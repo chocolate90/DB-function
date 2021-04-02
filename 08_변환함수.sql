a81ee5e0ef0e053444ac59b8ee28906dfe066f0c
chocolate90 <74293804+chocolate90@users.noreply.github.com>
2021-04-02 22:45:53 +0900

    변환함수 예제

--- 08_변환함수.sql
+++ 08_변환함수.sql
@@ -74,7 +74,7 @@ select first_name,
 from employees;
 
 
---CASE 컬럼 WHEN 값 THEN 결과 END
+--CASE 컬럼 WHEN 값 THEN 결과 END - ★★★★★
 select first_name,
        salary,
        job_id,
@@ -83,3 +83,41 @@ select first_name,
                     else salary * 0.1
         end) as bonus
 from employees;
+
+
+--연습문제
+
+--문제 1.
+--현재일자를 기준으로 EMPLOYEE테이블의 입사일자(hire_date)를 참조해서 근속년수가 10년 이상인
+--사원을 다음과 같은 형태의 결과를 출력하도록 쿼리를 작성해 보세요.
+--조건 1) 근속년수가 높은 사원 순서대로 결과가 나오도록 합니다
+select * from employees;
+
+select employee_id as 사원번호,
+       concat(first_name || ' ', last_name) as 사원명,
+       hire_date as 입사일자,
+       trunc((sysdate - hire_date)/365) as 근속년수
+from employees
+where trunc((sysdate - hire_date)/365) >= 10
+order by 근속년수 desc;
+
+--문제 2.
+--EMPLOYEE 테이블의 manager_id컬럼을 확인하여 first_name, manager_id, 직급을 출력합니다.
+--100이라면 ‘사원’,
+--120이라면 ‘주임’
+--121이라면 ‘대리’
+--122라면 ‘과장’
+--나머지는 ‘임원’ 으로 출력합니다.
+--조건 1) department_id가 50인 사람들을 대상으로만 조회합니다
+
+select first_name,
+       manager_id,
+       (case manager_id when 100 then '사원'
+                        when 120 then '주임'
+                        when 121 then '대리'
+                        when 122 then '과장'
+                        else '임원'
+       
+       end) as 직급
+from employees
+where department_id = 50;
